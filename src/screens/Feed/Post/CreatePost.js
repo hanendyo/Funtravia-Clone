7a547e623e7ec98ea0d3c1c18a575ee625ad0afa
Asep IM <asepim19@gmail.com>
2021-02-02 21:08:20 +0700

    02022021

--- src/screens/Feed/Post/CreatePost.js
+++ src/screens/Feed/Post/CreatePost.js
@@ -9,6 +9,7 @@ import {
 	Keyboard,
 	Alert,
 	Image,
+	SafeAreaView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { default_image } from "../../../assets/png";
@@ -283,151 +284,162 @@ export default function CreatePost(props) {
 			behavior={Platform.OS === "ios" ? "padding" : null}
 			style={{ flex: 1, backgroundColor: "#FFFFFF" }}
 		>
-			<View style={{ backgroundColor: "#209FAE", height: 55 }}>
-				<View
-					style={{
-						flexDirection: "row",
-						justifyContent: "flex-end",
-						alignItems: "center",
-						alignContent: "center",
-						height: 55,
-					}}
-				>
-					<Button size="medium" text="Post" onPress={() => SubmitData()} />
+			<SafeAreaView>
+				<View style={{ backgroundColor: "#209FAE", height: 55 }}>
+					<View
+						style={{
+							flexDirection: "row",
+							justifyContent: "flex-end",
+							alignItems: "center",
+							alignContent: "center",
+							height: 55,
+						}}
+					>
+						<Button size="medium" text="Post" onPress={() => SubmitData()} />
+					</View>
 				</View>
-			</View>
-			<ScrollView style={{}}>
-				<Loading show={loadingok} />
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<View>
-						<AutoHeightImage
-							width={Dimensions.get("window").width}
-							source={
-								chosenPicture && chosenPicture.uri
-									? {
-											uri: chosenPicture.uri,
-									  }
-									: default_image
-							}
-						/>
-						{/* </View> */}
-
-						<View
-							style={{
-								flexDirection: "row",
-								backgroundColor: "#ffffff",
-								borderBottomColor: "#f0f0f0f0",
-								borderBottomWidth: 1,
-							}}
-						>
-							<Image
+				<ScrollView style={{}}>
+					<Loading show={loadingok} />
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+						<View>
+							<AutoHeightImage
+								width={Dimensions.get("screen").width}
 								source={
-									dataprofile &&
-									(dataprofile.user_profile !== undefined || null || "")
-										? { uri: dataprofile.user_profile.picture }
+									chosenPicture && chosenPicture.path
+										? {
+												uri: chosenPicture.path,
+										  }
 										: default_image
 								}
-								style={{
-									width: 50,
-									height: 50,
-									borderRadius: 50,
-									margin: 10,
-									marginRight: 15,
-								}}
 							/>
-							<TextInput
-								multiline
-								placeholder={"Write a caption.."}
-								maxLength={255}
-								style={{
-									height: 75,
-									width: Dimensions.get("screen").width - 100,
-								}}
-								onChangeText={(text) => _setStatusText(text)}
-								value={statusText}
-							/>
-						</View>
-						<View
-							style={{
-								flexDirection: "row",
-								alignItems: "center",
-								marginTop: 10,
-								marginHorizontal: 15,
-							}}
-						>
-							<Ripple
-								onPress={() => setModellocation(true)}
+							<View
 								style={{
 									flexDirection: "row",
-
-									alignItems: "center",
+									backgroundColor: "#ffffff",
+									borderBottomColor: "#f0f0f0f0",
+									borderBottomWidth: 1,
 								}}
 							>
-								{Location.latitude !== "" ? (
-									<PinHijau height={15} width={15} />
-								) : null}
-								<Text
-									type="bold"
-									size="description"
+								<Image
+									source={
+										dataprofile &&
+										(dataprofile.user_profile !== undefined || null || "")
+											? { uri: dataprofile.user_profile.picture }
+											: default_image
+									}
 									style={{
-										marginHorizontal: 5,
+										width: 50,
+										height: 50,
+										borderRadius: 50,
+										margin: 10,
+										marginRight: 15,
 									}}
-								>
-									{Location.address}
-								</Text>
-							</Ripple>
-							{Location.latitude !== "" ? (
+								/>
+								<TextInput
+									multiline
+									placeholder={"Write a caption.."}
+									maxLength={255}
+									style={
+										Platform.OS == "ios"
+											? {
+													height: 75,
+													width: Dimensions.get("screen").width - 100,
+													maxHeight: 100,
+													marginVertical: 5,
+													marginHorizontal: 10,
+													paddingTop: 10,
+											  }
+											: {
+													height: 75,
+													width: Dimensions.get("screen").width - 100,
+											  }
+									}
+									onChangeText={(text) => _setStatusText(text)}
+									value={statusText}
+								/>
+							</View>
+							<View
+								style={{
+									flexDirection: "row",
+									alignItems: "center",
+									marginTop: 10,
+									marginHorizontal: 15,
+								}}
+							>
 								<Ripple
-									onPress={() => clearLoaction()}
+									onPress={() => setModellocation(true)}
 									style={{
-										padding: 5,
+										flexDirection: "row",
+
+										alignItems: "center",
 									}}
 								>
-									<Xgray height={15} width={15} />
-								</Ripple>
-							) : null}
-						</View>
-						<View
-							style={{
-								borderBottomColor: "#E1E1E1",
-								borderBottomWidth: 1,
-								width: 150,
-								margin: 10,
-							}}
-						></View>
-						<View
-							style={{
-								width: "100%",
-								flexWrap: "wrap",
-								flexDirection: "row",
-								paddingHorizontal: 10,
-							}}
-						>
-							{datanearby.map((value, index) => {
-								return index < 5 ? (
-									<Button
-										onPress={() => _selectLocation(value)}
-										type="box"
-										variant="bordered"
-										color="primary"
-										size="small"
+									{Location.latitude !== "" ? (
+										<PinHijau height={15} width={15} />
+									) : null}
+									<Text
+										type="bold"
+										size="description"
 										style={{
-											marginHorizontal: 2,
-											marginVertical: 2,
+											marginHorizontal: 5,
 										}}
-										text={value.name}
-									></Button>
-								) : null;
-							})}
+									>
+										{Location.address}
+									</Text>
+								</Ripple>
+								{Location.latitude !== "" ? (
+									<Ripple
+										onPress={() => clearLoaction()}
+										style={{
+											padding: 5,
+										}}
+									>
+										<Xgray height={15} width={15} />
+									</Ripple>
+								) : null}
+							</View>
+							<View
+								style={{
+									borderBottomColor: "#E1E1E1",
+									borderBottomWidth: 1,
+									width: 150,
+									margin: 10,
+								}}
+							></View>
+							<View
+								style={{
+									width: "100%",
+									flexWrap: "wrap",
+									flexDirection: "row",
+									paddingHorizontal: 10,
+								}}
+							>
+								{datanearby.map((value, index) => {
+									return index < 5 ? (
+										<Button
+											onPress={() => _selectLocation(value)}
+											type="box"
+											variant="bordered"
+											color="primary"
+											size="small"
+											style={{
+												marginHorizontal: 2,
+												marginVertical: 2,
+											}}
+											text={value.name}
+										></Button>
+									) : null;
+								})}
+							</View>
 						</View>
-					</View>
-				</TouchableWithoutFeedback>
-			</ScrollView>
-			<LocationSelector
-				modals={modellocation}
-				setModellocation={(e) => setModellocation(e)}
-				masukan={(e) => _setLocation(e)}
-			/>
+					</TouchableWithoutFeedback>
+				</ScrollView>
+				<LocationSelector
+					modals={modellocation}
+					setModellocation={(e) => setModellocation(e)}
+					masukan={(e) => _setLocation(e)}
+				/>
+			</SafeAreaView>
 		</KeyboardAvoidingView>
 	);
 }
