788c24fbb689f500d0f648b0e74b6761bc62ceaa
ciko <carsikoc@gmail.com>
2021-02-22 11:01:21 +0700

    journal comments

---
+++ src/screens/Journal/AddComment.js
@@ -0,0 +1,274 @@
+import { View } from "native-base";
+import { Dimensions, Alert, Keyboard } from "react-native";
+import React, { useState } from "react";
+import { LikeJournal, Shareout, CommentChat, LikeRed } from "../../assets/svg";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { Loading } from "../../component";
+import { useTranslation } from "react-i18next";
+import Liked from "../../graphQL/Mutation/Journal/likedJournal";
+import UnLiked from "../../graphQL/Mutation/Journal/unlikedJournal";
+import AddCommentJournal from "../../graphQL/Mutation/Journal/AddCommentJournal";
+import { useMutation } from "@apollo/react-hooks";
+
+export default function AddComment({ data, token, fetchData, listComments }) {
+  let [dataList, setDataList] = useState(data);
+  let [text, setText] = useState("");
+  const { t } = useTranslation();
+  const [setting, setSetting] = useState("");
+  const [
+    mutationliked,
+    { loading: loadingLike, data: dataLike, error: errorLike },
+  ] = useMutation(Liked, {
+    context: {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  });
+
+  const [
+    mutationUnliked,
+    { loading: loadingUnLike, data: dataUnLike, error: errorUnLike },
+  ] = useMutation(UnLiked, {
+    context: {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  });
+
+  const [
+    MutationAddComment,
+    { loading: loadingComment, data: dataListComment, error: errorComment },
+  ] = useMutation(AddCommentJournal, {
+    context: {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  });
+
+  const comment = async (id, text) => {
+    console.log(id, text);
+
+    Keyboard.dismiss();
+    if ((token || token !== "") && text !== "") {
+      try {
+        let response = await MutationAddComment({
+          variables: {
+            id: id,
+            text: text,
+          },
+        });
+        if (loadingLike) {
+          Alert.alert("Loading!!");
+        }
+        if (errorLike) {
+          throw new Error("Error Input");
+        }
+        if (response.data) {
+          if (
+            response.data.comment_journal.code === 200 ||
+            response.data.comment_journal.code === "200"
+          ) {
+            setText("");
+            listComments();
+          } else {
+            throw new Error(response.data.comment_journal.message);
+          }
+        }
+      } catch (error) {
+        Alert.alert("" + error);
+      }
+    } else {
+      Alert.alert("Please Insert a Text");
+    }
+  };
+
+  const _liked = async (id) => {
+    if (token || token !== "") {
+      try {
+        let response = await mutationliked({
+          variables: {
+            id: id,
+          },
+        });
+        if (loadingLike) {
+          Alert.alert("Loading!!");
+        }
+        if (errorLike) {
+          throw new Error("Error Input");
+        }
+        if (response.data) {
+          if (
+            response.data.like_journal.code === 200 ||
+            response.data.like_journal.code === "200"
+          ) {
+            var tempData = { ...dataList };
+            tempData.liked = true;
+            setDataList(tempData);
+            fetchData();
+          } else {
+            throw new Error(response.data.like_journal.message);
+          }
+        }
+      } catch (error) {
+        Alert.alert("" + error);
+      }
+    } else {
+      Alert.alert("Please Login");
+    }
+  };
+
+  const _unliked = async (id) => {
+    if (token || token !== "") {
+      try {
+        let response = await mutationUnliked({
+          variables: {
+            id: id,
+          },
+        });
+        if (loadingUnLike) {
+          Alert.alert("Loading!!");
+        }
+        if (errorUnLike) {
+          throw new Error("Error Input");
+        }
+        if (response.data) {
+          if (
+            response.data.unlike_journal.code === 200 ||
+            response.data.unlike_journal.code === "200"
+          ) {
+            var tempData = { ...dataList };
+            tempData.liked = false;
+            setDataList(tempData);
+            fetchData();
+          } else {
+            throw new Error(response.data.unlike_journal.message);
+          }
+        }
+      } catch (error) {
+        Alert.alert("" + error);
+      }
+    } else {
+      Alert.alert("Please Login");
+    }
+  };
+
+  const arrayShadow = {
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: Platform.OS == "ios" ? 0.22 : 2,
+    shadowRadius: Platform.OS == "ios" ? 2.22 : 1.0,
+    elevation: Platform.OS == "ios" ? 3 : 1.5,
+  };
+
+  const loadAsync = async () => {
+    let tkn = await AsyncStorage.getItem("access_token");
+    await setToken(tkn);
+    let setsetting = await AsyncStorage.getItem("setting");
+    await setSetting(JSON.parse(setsetting));
+  };
+
+  return (
+    <View
+      style={{
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingHorizontal: 20,
+        width: Dimensions.get("window").width,
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "white",
+        borderWidth: 2,
+        borderColor: "#F0F0F0",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: arrayShadow.shadowOpacity,
+        shadowRadius: arrayShadow.shadowRadius,
+        elevation: arrayShadow.elevation,
+      }}
+    >
+      {/* <Loading show={loadingLike} />
+      <Loading show={loadingUnLike} /> */}
+      <View
+        style={{
+          backgroundColor: "#f6f6f6",
+          borderRadius: 20,
+          width: "100%",
+          height: Dimensions.get("window").width * 0.13,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          alignSelf: "center",
+        }}
+      >
+        <TextInput
+          style={{
+            flex: 1,
+            fontWeight: "bold",
+            marginLeft: 5,
+            width: "90%",
+            flexWrap: "wrap",
+            color: "#2c2c2c",
+          }}
+          onChangeText={(text) => setText(text)}
+          value={text}
+          placeholder={t("writeComment")}
+          returnKeyType="default"
+          onSubmitEditing={() => comment(dataList.id, text)}
+        />
+      </View>
+      {/* <TouchableOpacity onPress={() => Alert.alert("Comming Soon")}>
+        <View
+          style={{
+            marginLeft: 10,
+            borderRadius: 30,
+            backgroundColor: "#f6f6f6",
+            width: Dimensions.get("window").width * 0.13,
+            height: Dimensions.get("window").width * 0.13,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Shareout width={20} height={20} />
+        </View>
+      </TouchableOpacity>
+      {data?.liked === false ? (
+        <TouchableOpacity onPress={() => _liked(dataList.id)}>
+          <View
+            style={{
+              marginLeft: 10,
+              borderRadius: 30,
+              backgroundColor: "#f6f6f6",
+              width: Dimensions.get("window").width * 0.13,
+              height: Dimensions.get("window").width * 0.13,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <LikeJournal width={20} height={20} />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={() => _unliked(dataList.id)}>
+          <View
+            style={{
+              marginLeft: 10,
+              borderRadius: 30,
+              backgroundColor: "#f6f6f6",
+              width: Dimensions.get("window").width * 0.13,
+              height: Dimensions.get("window").width * 0.13,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <LikeRed width={20} height={20} />
+          </View>
+        </TouchableOpacity>
+      )} */}
+    </View>
+  );
+}
