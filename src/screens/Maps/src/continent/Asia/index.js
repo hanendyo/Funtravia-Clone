66be81a3d79687ecc030209c2c9aea8e1f29f72a
Asep IM <asepim19@gmail.com>
2021-03-02 20:36:43 +0700

    destination maps

--- src/screens/Maps/src/continent/Asia/index.js
+++ src/screens/Maps/src/continent/Asia/index.js
@@ -2,27 +2,37 @@ import React from "react";
 import { useState } from "react";
 import { SafeAreaView, FlatList, TouchableOpacity, View } from "react-native";
 import Maps from "./maps";
+import { Country } from "../../../data/country";
 import { Text } from "../../../../../component";
 export default function Asia() {
 	const [changeColor, setChangeColor] = useState("#209FAE");
 	const [defaultColor, setDefaultColor] = useState("#DAF0F2");
-	const [change, setChange] = useState(null);
-	const data = [
-		{ id: "south", label: "South Asia" },
-		{ id: "southeast", label: "Southeast Asia" },
-		{ id: "north", label: "North Asia" },
-		{ id: "east", label: "East Asia" },
-		{ id: "western", label: "Western Asia" },
-		{ id: "all", label: "All" },
+	const [subContinent, setSubContinent] = useState({});
+	const subContinentData = [
+		{ id: "035", label: "South-eastern Asia" },
+		{ id: "143", label: "Central Asia" },
+		{ id: "034", label: "Southern Asia" },
+		{ id: "030", label: "Eastern Asia" },
+		{ id: "145", label: "Western Asia" },
+		{ id: "142", label: "All" },
 	];
+
+	const getCountry = (data) => {
+		if (subContinent.id === "142") {
+			return data["region-code"] === subContinent.id;
+		} else {
+			return data["sub-region-code"] === subContinent.id;
+		}
+	};
+
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<View style={{ backgroundColor: "#FFF" }}>
 				<Maps
-					changeId={change}
+					subContinent={subContinent}
 					colorChange={changeColor}
 					defaultColor={defaultColor}
-					setChange={(id) => setChange(id)}
+					setChange={(data) => setSubContinent(data)}
 				/>
 			</View>
 			<View
@@ -39,14 +49,14 @@ export default function Asia() {
 					margin: 10,
 				}}
 			>
-				{data.map((item, index) => (
+				{subContinentData.map((item, index) => (
 					<TouchableOpacity
 						key={index}
-						onPress={() => setChange(item.id)}
+						onPress={() => setSubContinent(item)}
 						style={{
-							backgroundColor: change === item.id ? "#209FAE" : "#FFF",
+							backgroundColor: subContinent.id === item.id ? "#209FAE" : "#FFF",
 							borderWidth: 0.5,
-							borderColor: change === item.id ? "#209FAE" : "#D1D1D1",
+							borderColor: subContinent.id === item.id ? "#209FAE" : "#D1D1D1",
 							paddingVertical: 8,
 							paddingHorizontal: 15,
 							borderRadius: 5,
@@ -57,19 +67,29 @@ export default function Asia() {
 							type="bold"
 							size="description"
 							style={{
-								color: change === item.id ? "#FFF" : "#464646",
+								color: subContinent.id === item.id ? "#FFF" : "#464646",
 							}}
 						>{`${item.label}`}</Text>
 					</TouchableOpacity>
 				))}
 			</View>
-			{/* <FlatList
-				data={data}
-				horizontal
+			<FlatList
+				data={Country.filter(getCountry)}
 				renderItem={({ item }) => (
-
+					<View
+						style={{
+							paddingVertical: 15,
+							paddingHorizontal: 15,
+							backgroundColor: "#FFF",
+							marginVertical: 5,
+							borderRadius: 5,
+						}}
+					>
+						<Text>{item.name}</Text>
+					</View>
 				)}
-			/> */}
+				contentContainerStyle={{ marginHorizontal: 15 }}
+			/>
 		</SafeAreaView>
 	);
 }
