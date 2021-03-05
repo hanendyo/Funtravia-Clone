7877db615b07ff693d7a737e04c8f766d60b3b05
ganjar <ganjarpuja94@gmail.com>
2021-03-05 17:11:30 +0700

    menu new

--- src/screens/Profile/OtherProfiles/index.js
+++ src/screens/Profile/OtherProfiles/index.js
@@ -1,37 +1,60 @@
+import { useQuery } from "@apollo/client";
 import React, { useEffect, useRef, useState } from "react";
 import {
-	ScrollView,
-	View,
-	SafeAreaView,
-	StatusBar,
-	Text,
-	Dimensions,
+  ScrollView,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Dimensions,
 } from "react-native";
-export default function OtherProfiles() {
-	return (
-		<View>
-			<View
-				style={{
-					backgroundColor: "#FFF",
-					height: Platform.OS === "ios" ? 44 : 50,
-				}}
-			>
-				<SafeAreaView>
-					<StatusBar
-						translucent
-						backgroundColor={"#000"}
-						barStyle="dark-content"
-					/>
-				</SafeAreaView>
-			</View>
-			<ScrollView>
-				<View>
-					<Text>PROFILE</Text>
-				</View>
-				<View>
-					<Text>CONTENT</Text>
-				</View>
-			</ScrollView>
-		</View>
-	);
+import Account from "../../../graphQL/Query/Home/Account";
+export default function OtherProfiles(props) {
+  console.log(props.route.params);
+
+  const Datauser = () => {
+    const { data, loading, error, refetch } = useQuery(Account, {
+      fetchPolicy: "network-only",
+      context: {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      variables: {
+        id: props.route.params.idUser,
+      },
+    });
+
+    console.log(data);
+
+    return (
+      <View>
+        <View
+          style={{
+            backgroundColor: "#FFF",
+            height: Platform.OS === "ios" ? 44 : 50,
+          }}
+        >
+          <SafeAreaView>
+            <StatusBar
+              translucent
+              backgroundColor={"#000"}
+              barStyle="dark-content"
+            />
+          </SafeAreaView>
+        </View>
+        <ScrollView>
+          <View>
+            <Text>PROFILE</Text>
+          </View>
+          <View>
+            <Text>CONTENT</Text>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  };
+
+  return <Datauser />;
 }
