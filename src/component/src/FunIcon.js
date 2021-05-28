23c9394c47877770063af7255ff396974df77d2a
Asep IM <asepim19@gmail.com>
2021-05-28 13:59:07 +0700

    CACHE & GRADLE ERROR FACEBOOK SDK

--- src/component/src/FunIcon.js
+++ src/component/src/FunIcon.js
@@ -7,53 +7,54 @@ import { ICONSERVER } from "../../config";
 import { SvgUri } from "react-native-svg";
 
 export default function FunIcon({
-  icon,
-  style = null,
-  width = null,
-  height = null,
-  fill = null,
+	icon,
+	style = null,
+	width = null,
+	height = null,
+	fill = null,
 }) {
-  const variant = { i: "icon", g: "icon", w: "weather", f: "flag", m: "map" };
-  const url = `${ICONSERVER}${variant[icon.charAt(0)]}/${
-    icon.split("-")[1]
-  }.svg`;
-  let [loading, setLoading] = useState(true);
-  const extension = Platform.OS === "android" ? "file://" : "";
-  const name = sh.unique(url);
-  const path = `${extension}${RNFS.CachesDirectoryPath}/${name}.svg`;
-  RNFS.exists(path)
-    .then((exists) => {
-      if (!exists) {
-        setLoading(true);
-        RNFS.downloadFile({
-          fromUrl: url,
-          toFile: path,
-        }).promise.then((res) => {
-          setLoading(false);
-        });
-      } else {
-        setLoading(false);
-      }
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
+	const variant = { i: "icon", g: "icon", w: "weather", f: "flag", m: "map" };
+	const url = `${ICONSERVER}${variant[icon.charAt(0)]}/${
+		icon.split("-")[1]
+	}.svg`;
+	let [loading, setLoading] = useState(false);
+	const extension = Platform.OS === "android" ? "file://" : "";
+	const name = sh.unique(url);
+	const path = `${extension}${RNFS.CachesDirectoryPath}/${name}.svg`;
+	console.log("ICON", url);
+	// RNFS.exists(path)
+	// 	.then((exists) => {
+	// 		if (!exists) {
+	// 			setLoading(true);
+	// 			RNFS.downloadFile({
+	// 				fromUrl: url,
+	// 				toFile: path,
+	// 			}).promise.then((res) => {
+	// 				setLoading(false);
+	// 			});
+	// 		} else {
+	// 			setLoading(false);
+	// 		}
+	// 	})
+	// 	.catch((error) => {
+	// 		console.warn(error);
+	// 	});
 
-  if (loading) {
-    return (
-      <SkeletonPlaceholder speed={500}>
-        <SkeletonPlaceholder.Item {...style} />
-      </SkeletonPlaceholder>
-    );
-  }
+	if (loading) {
+		return (
+			<SkeletonPlaceholder speed={500}>
+				<SkeletonPlaceholder.Item {...style} />
+			</SkeletonPlaceholder>
+		);
+	}
 
-  return (
-    <SvgUri
-      style={style ? style : null}
-      uri={path}
-      width={width ? width : 50}
-      height={height ? height : 50}
-      fill={fill ? fill : "#000"}
-    />
-  );
+	return (
+		<SvgUri
+			style={style ? style : null}
+			uri={url}
+			width={width ? width : 50}
+			height={height ? height : 50}
+			fill={fill ? fill : "#000"}
+		/>
+	);
 }
