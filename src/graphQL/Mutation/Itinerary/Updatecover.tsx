bcfb3b3a87fce56768d835b59e164b6c4557e285
ganjarpuja94 <ganjarpuja94@gmail.com>
2021-06-03 09:18:40 +0700

    filter

--- src/graphQL/Mutation/Itinerary/Updatecover.tsx
+++ src/graphQL/Mutation/Itinerary/Updatecover.tsx
@@ -1,12 +1,12 @@
-import { gql } from 'apollo-boost';
+import { gql } from "apollo-boost";
 const Updatecover = gql`
-	mutation($itinerary_id: ID!, $cover: String!) {
-		update_cover_itinerary(itinerary_id: $itinerary_id, cover: $cover) {
-			id
-			response_time
-			message
-			code
-		}
-	}
+  mutation($itinerary_id: ID!, $cover: String!) {
+    update_cover_itinerary(itinerary_id: $itinerary_id, cover: $cover) {
+      id
+      response_time
+      message
+      code
+    }
+  }
 `;
 export default Updatecover;
---
+++ src/graphQL/Query/Itinerary/SearchDestination.tsx
@@ -0,0 +1,19 @@
+import { gql } from "apollo-boost";
+const City = gql`
+  query($keyword: String, $cities_id: ID, $province_id: ID, $countries_id: ID) {
+    searchlocation_populer(
+      keyword: $keyword
+      cities_id: $cities_id
+      province_id: $province_id
+      countries_id: $countries_id
+    ) {
+      id
+      name
+      cover
+      type
+      head1
+      head2
+    }
+  }
+`;
+export default City;
