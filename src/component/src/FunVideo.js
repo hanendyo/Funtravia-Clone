7f40499f448484a030566d31ec9079898a500cb0
envy <faridyusman02@gmail.com>
2021-06-14 09:49:58 +0700

    notifikasi push navigate

--- src/component/src/FunVideo.js
+++ src/component/src/FunVideo.js
@@ -6,40 +6,41 @@ import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import Video from "react-native-video";
 
 export default function FunVideo({ style, poster, ...otherProps }) {
-	let [loading, setLoading] = useState(false);
-	let [temp, setTemp] = useState([]);
-	let uri = poster;
-	let path;
-	if (uri && uri !== undefined) {
-		let extension = Platform.OS === "android" ? "file://" : "";
-		let name = sh.unique(uri);
-		path = `${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
-		RNFS.exists(path)
-			.then((exists) => {
-				if (!exists && temp.indexOf(name) === -1) {
-					setLoading(true);
-					setTemp([...temp, name]);
-					RNFS.downloadFile({ fromUrl: uri, toFile: path }).promise.then(
-						(res) => {
-							setTimeout(() => setLoading(false), 1000);
-							console.log("SUCCESS CACHE IMAGE FROM VIDEO", uri);
-						}
-					);
-				}
-			})
-			.catch((error) => {
-				setLoading(false);
-				console.warn(error);
-			});
-	}
+    let [loading, setLoading] = useState(false);
+    let [temp, setTemp] = useState([]);
+    let uri = poster;
+    let path;
+    if (uri && uri !== undefined) {
+        let extension = Platform.OS === "android" ? "file://" : "";
+        let name = sh.unique(uri);
+        path = `${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
+        RNFS.exists(path)
+            .then((exists) => {
+                if (!exists && temp.indexOf(name) === -1) {
+                    setLoading(true);
+                    setTemp([...temp, name]);
+                    RNFS.downloadFile({
+                        fromUrl: uri,
+                        toFile: path,
+                    }).promise.then((res) => {
+                        setTimeout(() => setLoading(false), 1000);
+                        // console.log("SUCCESS CACHE IMAGE FROM VIDEO", uri);
+                    });
+                }
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.warn(error);
+            });
+    }
 
-	if (loading) {
-		return (
-			<SkeletonPlaceholder speed={500}>
-				<SkeletonPlaceholder.Item {...style} />
-			</SkeletonPlaceholder>
-		);
-	}
+    if (loading) {
+        return (
+            <SkeletonPlaceholder speed={500}>
+                <SkeletonPlaceholder.Item {...style} />
+            </SkeletonPlaceholder>
+        );
+    }
 
-	return <Video {...otherProps} style={style} poster={path} />;
+    return <Video {...otherProps} style={style} poster={path} />;
 }
