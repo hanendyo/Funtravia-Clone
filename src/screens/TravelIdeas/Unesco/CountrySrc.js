6d783e738b8866f287d509a0ecfc50014cf7c1ca
Ardi Cahyadi Yudistira <ardicahyadi20@gmail.com>
2021-08-03 17:52:42 +0700

    Penambahan filter destination unesco

--- src/screens/TravelIdeas/Unesco/CountrySrc.js
+++ src/screens/TravelIdeas/Unesco/CountrySrc.js
@@ -7,9 +7,18 @@ import {
   Platform,
   TextInput,
   Pressable,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Arrowbackwhite, IdFlag, Check, Search } from "../../../assets/svg";
+import {
+  Arrowbackwhite,
+  IdFlag,
+  Check,
+  Search,
+  Filternewbiru,
+  Xhitam,
+} from "../../../assets/svg";
 import Modal from "react-native-modal";
 import { Text, Button, FunIcon } from "../../../component";
 import Ripple from "react-native-material-ripple";
@@ -17,6 +26,8 @@ import { useTranslation } from "react-i18next";
 import { useMutation, useQuery } from "@apollo/react-hooks";
 import CountryListSrcUnesco from "../../../graphQL/Query/Countries/CountryListSrcUnesco";
 import ContinentList from "../../../graphQL/Query/Countries/ContinentList";
+import CheckBox from "@react-native-community/checkbox";
+import { TemporaryDirectoryPath } from "react-native-fs";
 
 export default function CountrySrc({
   selectedCountry,
@@ -27,11 +38,14 @@ export default function CountrySrc({
   const { t } = useTranslation();
   let [datacountry, setdataCountry] = useState(data);
   let [select_continent, setContinentSelected] = useState("");
-  console.log(select_continent);
+  let [select_continents, setContinentSelecteds] = useState([]);
   let [keyword, setKeyword] = useState("");
   let slider = useRef();
-
+  let [FilterContinent, setFilterContinent] = useState([]);
   let [continent_list, setDatacontinent] = useState([]);
+  let [continent_listfilter, setDatacontinentfilter] = useState([]);
+  // show filter
+  let [show, setshow] = useState(false);
 
   const {
     data: datacontinent,
@@ -44,16 +58,16 @@ export default function CountrySrc({
     },
     onCompleted: () => {
       continent_list = setDatacontinent(datacontinent.continent_list);
+      setDatacontinentfilter(datacontinent.continent_list);
     },
   });
 
   const { data, loading, error, refetch } = useQuery(CountryListSrcUnesco, {
     variables: {
-      continent_id: select_continent ? [select_continent] : null,
+      continent_id: select_continents ? select_continents : null,
       keyword: keyword,
     },
   });
-  console.log("continent", data);
 
   let list_country_src_unesco = [];
   if (data && data.list_country_src_unesco) {
@@ -78,6 +92,74 @@ export default function CountrySrc({
     }
   };
 
+  // cek data filter
+
+  const cekData = (data) => {
+    let dat = continent_list.filter((k) => k.checked === true);
+
+    return dat.length;
+  };
+
+  // set filter
+
+  const searchcontinentfilter = async (teks) => {
+    setKeyword(teks);
+    let searching = new RegExp(teks, "i");
+    let Continent = continent_list.filter((item) => searching.test(item.name));
+    setDatacontinentfilter(Continent);
+  };
+
+  // checkbox handle
+
+  const _handleCheck = async (id, index, item) => {
+    let tempe = [...continent_listfilter];
+    let items = { ...item };
+    items.checked = !items.checked;
+    let inde = tempe.findIndex((key) => key.id === id);
+    tempe.splice(inde, 1, items);
+    await setDatacontinentfilter(tempe);
+    await setDatacontinent(tempe);
+
+    // await setdataFilterCategoris(tempe);
+  };
+
+  console.log("continent", continent_listfilter);
+
+  // update filter handle
+  const UpdateFilter = async () => {
+    let idselected = [];
+    let tempdatasfilter = [...continent_listfilter];
+    for (var i in tempdatasfilter) {
+      if (tempdatasfilter[i].checked == true) {
+        tempdatasfilter[i].show = true;
+        tempdatasfilter.push(tempdatasfilter[i]);
+        idselected.push(tempdatasfilter[i].id);
+      }
+    }
+    await setContinentSelecteds(idselected);
+    await setshow(false);
+  };
+
+  const ClearAllFilter = () => {
+    let tempe = [...continent_listfilter];
+    let tempes = [];
+    for (var x of tempe) {
+      let data = { ...x };
+      data.checked = false;
+      data.show = false;
+      tempes.push(data);
+    }
+
+    // let tempdatascountry = [...dataFilterCountry];
+    // for (var i in tempdatascountry) {
+    //   tempdatascountry[i].checked = false;
+    //   tempdatascountry[i].show = false;
+    // }
+
+    setContinentSelecteds([]);
+    setDatacontinent([]);
+    setDatacontinentfilter(tempes);
+  };
   return (
     <Modal
       onRequestClose={() => {
@@ -136,7 +218,6 @@ export default function CountrySrc({
             width: Dimensions.get("screen").width,
             height: Dimensions.get("screen").height - 55,
             backgroundColor: "white",
-            // paddingBottom: 20,
           }}
         >
           <View
@@ -152,30 +233,66 @@ export default function CountrySrc({
               shadowOpacity: 0.25,
               shadowRadius: 1,
               elevation: 3,
-              // borderBottomColor: "#d3d3d3",
-              // borderBottomWidth: 1,
             }}
           >
             <View
               style={{
-                alignContent: "center",
-                alignItems: "center",
-                justifyContent: "center",
+                flexDirection: "row",
+                paddingHorizontal: 15,
                 paddingVertical: 10,
-                backgroundColor: "white",
-                // width: Dimensions.get("screen").width,
+                backgroundColor: "#fff",
               }}
             >
+              <Button
+                size="small"
+                type="icon"
+                variant="bordered"
+                color="primary"
+                onPress={() => {
+                  setshow(true);
+                }}
+                style={{
+                  marginRight: 5,
+                  borderRadius: 3,
+                  paddingHorizontal: 10,
+                  borderColor: "#209FAE",
+                  paddingBottom: 1,
+                }}
+              >
+                <Filternewbiru width={18} height={18} />
+                {cekData() > 0 ? (
+                  <View
+                    style={{
+                      backgroundColor: "#209fae",
+                      marginLeft: 10,
+                      width: 20,
+                      paddingHorizontal: 5,
+                      borderRadius: 2,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "Lato-Regular",
+                        color: "#ffff",
+                        fontSize: 15,
+                        // alignSelf: "center",
+                      }}
+                    >
+                      {cekData(continent_list)}
+                    </Text>
+                  </View>
+                ) : null}
+              </Button>
               <View
                 style={{
-                  backgroundColor: "#DAF0F2",
-                  borderRadius: 5,
-                  width: Dimensions.get("window").width - 20,
-                  paddingHorizontal: 10,
+                  backgroundColor: "#F0F0F0",
+                  borderRadius: 3,
+                  flex: 1,
                   flexDirection: "row",
                   alignItems: "center",
                   alignContent: "center",
-                  height: 35,
+
+                  paddingHorizontal: 10,
                 }}
               >
                 <View
@@ -202,7 +319,7 @@ export default function CountrySrc({
                 />
               </View>
             </View>
-
+            {/* 
             <FlatList
               data={continent_list}
               horizontal={true}
@@ -241,7 +358,7 @@ export default function CountrySrc({
                   </Pressable>
                 );
               }}
-            />
+            /> */}
           </View>
           <FlatList
             ref={slider}
@@ -309,6 +426,250 @@ export default function CountrySrc({
           />
         </View>
       </View>
+      {/* modal filter */}
+      <Modal
+        onBackdropPress={() => {
+          setshow(false);
+        }}
+        onRequestClose={() => setshow(false)}
+        onDismiss={() => setshow(false)}
+        isVisible={show}
+        style={{
+          justifyContent: "flex-end",
+          margin: 0,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "column",
+            height: Dimensions.get("screen").height * 0.6,
+            width: Dimensions.get("screen").width,
+            backgroundColor: "white",
+          }}
+        >
+          {/* bagian atas filter*/}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingHorizontal: 15,
+              paddingVertical: 20,
+            }}
+          >
+            <Text
+              type="bold"
+              size="title"
+              style={{
+                color: "#464646",
+              }}
+            >
+              Filter
+            </Text>
+            <TouchableOpacity
+              style={{
+                position: "absolute",
+                backgroundColor: "with",
+                height: 35,
+                width: 32,
+                top: 0,
+                right: 0,
+                justifyContent: "flex-end",
+                alignContent: "flex-end",
+                alignItems: "flex-start",
+              }}
+              onPress={() => setshow(false)}
+            >
+              <Xhitam height={15} width={15} />
+            </TouchableOpacity>
+          </View>
+          {/* bagian content filter */}
+          <View
+            style={{
+              flexDirection: "row",
+              flex: 1,
+              borderTopWidth: 0.5,
+              borderColor: "#d1d1d1",
+            }}
+          >
+            {/* sidebar filter */}
+            <View
+              style={{
+                width: "35%",
+                borderRightWidth: 0.5,
+                borderColor: "#d1d1d1",
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#f6f6f6",
+                  paddingBottom: 5,
+                }}
+              >
+                <View
+                  style={{
+                    borderLeftColor: "#209fae",
+                    borderLeftWidth: 5,
+                    marginLeft: 5,
+                    justifyContent: "center",
+                    paddingVertical: 15,
+                    paddingHorizontal: 10,
+                    backgroundColor: "#ffff",
+                  }}
+                >
+                  <Text
+                    type="bold"
+                    size="title"
+                    style={{
+                      // fontSize: 20,
+                      // fontFamily: "Lato-Bold",
+                      color: "#464646",
+                      // marginTop: 10,
+                    }}
+                  >
+                    {t("continent")}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View style={{ flex: 1 }}>
+              <View
+                style={{
+                  padding: 15,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "#daf0f2",
+                    borderRadius: 5,
+                    // flex: 1,
+
+                    flexDirection: "row",
+                    alignItems: "center",
+                    alignContent: "center",
+                    paddingHorizontal: 10,
+                    paddingVertical: 5,
+                  }}
+                >
+                  <Search width={15} height={15} />
+
+                  <TextInput
+                    underlineColorAndroid="transparent"
+                    placeholder={t("search")}
+                    style={{
+                      width: "100%",
+                      // borderWidth: 1,
+                      marginLeft: 5,
+                      padding: 0,
+                    }}
+                    returnKeyType="search"
+                    onChangeText={(x) => searchcontinentfilter(x)}
+                    onSubmitEditing={(x) => searchcontinentfilter(x)}
+                  />
+                </View>
+              </View>
+              <ScrollView
+                nestedScrollEnabled={true}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                  paddingHorizontal: 15,
+                }}
+              >
+                {continent_listfilter.map((item, index) => (
+                  <TouchableOpacity
+                    onPress={() => _handleCheck(item["id"], index, item)}
+                    style={{
+                      flexDirection: "row",
+                      backgroundColor: "white",
+                      // borderColor: "#464646",
+                      width: "49%",
+                      marginRight: 3,
+                      marginBottom: 20,
+                      justifyContent: "flex-start",
+                      alignContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CheckBox
+                      onCheckColor="#FFF"
+                      lineWidth={1}
+                      onFillColor="#209FAE"
+                      onTintColor="#209FAE"
+                      boxType={"square"}
+                      style={{
+                        alignSelf: "center",
+                        width: Platform.select({
+                          ios: 30,
+                          android: 35,
+                        }),
+                        transform: Platform.select({
+                          ios: [{ scaleX: 0.8 }, { scaleY: 0.8 }],
+                          android: [{ scaleX: 1.3 }, { scaleY: 1.3 }],
+                        }),
+                      }}
+                      onValueChange={() =>
+                        _handleCheck(item["id"], index, item)
+                      }
+                      value={item["checked"]}
+                    />
+
+                    <Text
+                      size="label"
+                      type="regular"
+                      style={{
+                        marginLeft: 0,
+                        color: "#464646",
+                        // borderWidth: 5,
+                      }}
+                    >
+                      {item["name"]}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          </View>
+          {/* bagian footer modal filter */}
+          <View
+            style={{
+              flex: 1,
+              zIndex: 6,
+              flexDirection: "row",
+              height: 80,
+              position: "absolute",
+              bottom: 0,
+              justifyContent: "space-around",
+              alignContent: "center",
+              alignItems: "center",
+              backgroundColor: "#ffffff",
+              width: Dimensions.get("screen").width,
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+              padding: 10,
+              paddingHorizontal: 10,
+            }}
+          >
+            <Button
+              variant="bordered"
+              color="secondary"
+              onPress={() => ClearAllFilter()}
+              style={{ width: "30%", borderColor: "#ffff" }}
+              text={t("clearAll")}
+            ></Button>
+            <Button
+              onPress={() => UpdateFilter()}
+              style={{ width: "65%" }}
+              text={t("apply")}
+            ></Button>
+          </View>
+        </View>
+      </Modal>
     </Modal>
   );
 }
