d38226ef91faae10591b40c04ac1ae136a3df5a6
Yusman Farid <faridyusman02@gmail.com>
2021-08-12 10:46:57 +0700

    user travel album done

--- src/graphQL/Query/Profile/itinerary.tsx
+++ src/graphQL/Query/Profile/itinerary.tsx
@@ -1,30 +1,37 @@
-import { gql } from 'apollo-boost';
+import { gql } from "apollo-boost";
 const itinerary = gql`
-	query {
-		user_trip {
-			id
-			name
+  query {
+    user_trip {
+      id
+      name
 
-			cover
-			country {
-				name
-			}
-			city {
-				name
-			}
-			buddy {
-				id
-				user {
-					username
-					first_name
-					picture
-				}
-			}
-			start_date
-			end_date
-			isprivate
-			status
-		}
-	}
+      cover
+      country {
+        name
+      }
+      city {
+        name
+      }
+      buddy {
+        id
+        user {
+          username
+          first_name
+          picture
+        }
+      }
+      user_created {
+        id
+        username
+        first_name
+        last_name
+        picture
+      }
+      start_date
+      end_date
+      isprivate
+      status
+    }
+  }
 `;
 export default itinerary;
