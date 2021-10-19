29e50bbe3328e14523a0e89cff1f7087827ff800
Hanendyo Indira <hanendyo@Hanendyos-MacBook-Air.local>
2021-10-19 09:43:05 +0700

    event, itin event, $ language

--- src/component/src/CopyLink.js
+++ src/component/src/CopyLink.js
@@ -6,30 +6,35 @@ import Clipboard from "@react-native-clipboard/clipboard";
 import { useTranslation } from "react-i18next";
 
 export default async function CopyLink({ from, target }) {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
+  console.log("FROM: ", from);
+  console.log("TARGET: ", target);
 
-  const success = (t) => {
-    return t("linkCopiedToClipboard");
-  };
-  const failed = (t) => {
-    return t("failedToLinkCopiedToClipboard");
-  };
+  // const success = (t) => {
+  //   return t("linkCopiedToClipboard");
+  // };
+  // const failed = (t) => {
+  //   return t("failedToLinkCopiedToClipboard");
+  // };
 
-  let successCopy = t("linkCopiedToClipboard");
-  let failedCopy = t("failedToLinkCopiedToClipboard");
+  // let successCopy = t("linkCopiedToClipboard");
+  // let failedCopy = t("failedToLinkCopiedToClipboard");
+
   try {
     let response = await fetch(`${SHARE_LINK}/getlink/${from}/${target}`, {
       method: "GET",
       Accept: "application/json",
       "Content-Type": "application/json",
     });
-    console.log("response", response);
+
     let dataResponse = await response.json();
+
+    console.log("response", response);
     console.log("response", dataResponse);
 
     Clipboard.setString(dataResponse.result.link);
     RNToasty.Normal({
-      title: success(t),
+      // title: t("linkCopiedToClipboard"),
       title: "Link Copied To Clipboard",
       position: "bottom",
       // tintColor: rgb(32, 159, 174),
@@ -37,9 +42,8 @@ export default async function CopyLink({ from, target }) {
   } catch (error) {
     console.log(error);
     RNToasty.Show({
-      // title: "Failed To Copy Link To Clipboard",
-      title: failed(t),
-      title: "",
+      // title: t("failedToCopyLinkToClipboard"),
+      title: "Failed To Copy Link To Clipboard",
       position: "bottom",
     });
   }
