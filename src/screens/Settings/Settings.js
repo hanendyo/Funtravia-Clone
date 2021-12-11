9fd2ca522543c33c35216d4fc57193d73524affc
ciko <carsikoc@gmail.com>
2021-12-11 17:33:59 +0700

    itinerary name, event, itinerary search dan solve problem package

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -586,7 +586,7 @@ export default function Settings(props) {
               }}
             >
               <Text type="light" size="description" style={{}}>
-                1.0.26
+                1.0.32@1
               </Text>
             </View>
           </View>
